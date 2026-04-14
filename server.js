a93b6f2523c7eee78ddee455ab369620a838576e
Rupesh <dasrupesh2124@gmail.com>
2026-04-14 20:57:44 +0530

    Auth added

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3005, () => {
+    console.log('Server is running on port 3005');
 })
