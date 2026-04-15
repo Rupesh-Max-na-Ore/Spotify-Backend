902175e796b485505ec72c45ad9187718c788d07
Rupesh <dasrupesh2124@gmail.com>
2026-04-15 11:52:41 +0530

    Took Auth out of controller, put in middleware with req body mod for user id decode passing

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -5,73 +5,33 @@ const jwt = require('jsonwebtoken');
 
 async function createMusic(req, res) {
 
+    const { title } = req.body;
 
+    const file = req.file;
 
-    const token = req.cookies.token;
+    const result = await uploadFile(file.buffer.toString('base64'));
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    const music = await musicModel.create({
+        uri: result.url,
+        title,
+        artist: req.user.id,
+    })
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        if(!(decoded.role == 'artist')){
-            return res.status(403).json({message: 'You are not authorized to create music. Consider signing up as an artist.'});
-        }
-
-        const { title } = req.body;
-
-        const file = req.file;
-
-        const result = await uploadFile(file.buffer.toString('base64'));
-
-        const music = await musicModel.create({
-            uri: result.url,
-            title,
-            artist: decoded.id,
-        })
-
-        res.status(201).json({ message: 'Music created successfully', music }); 
-
-
-    } catch (error) {
-
-        console.log(error);
-        return res.status(401).json({ message: 'Invalid token/Unauthorized' });
-    }
-
+    res.status(201).json({ message: 'Music created successfully', music }); 
 
 }
 
 async function createAlbum(req, res) {
 
-    const token = req.cookies.token;
-    
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    const { title, musics } = req.body;
+    const album = await albumModel.create({
+        title,
+        artist: req.user.id,
+        musics: musics,
+    })
+    res.status(201).json({ message: 'Album created successfully', album: { id: album._id, title: album.title, artist: album.artist, musics: album.musics } });
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        if(!(decoded.role == 'artist')){
-            return res.status(403).json({message: 'You are not authorized to create album. Consider signing up as an artist.'});
-        }
-
-        const { title, musics } = req.body;
-        const album = await albumModel.create({
-            title,
-            artist: decoded.id,
-            musics: musics,
-        })
-        res.status(201).json({ message: 'Album created successfully', album: { id: album._id, title: album.title, artist: album.artist, musics: album.musics } });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(401).json({ message: 'Invalid token/Unauthorized' });
-    }
-    
+   
 }
 
 module.exports = {
