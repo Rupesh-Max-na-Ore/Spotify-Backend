fd13474d8f1c286e901ad4a0a02925b3510533a2
Rupesh <dasrupesh2124@gmail.com>
2026-04-15 12:37:59 +0530

    Added logout api

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,4 +7,8 @@ router.post('/register', authController.registerUser);
 
 router.post('/login', authController.loginUser);
 
+// logout route for users to clear the token cookie and log out
+// in production, token blacklisting is used
+router.post('/logout', authController.logoutUser);
+
 module.exports = router;
